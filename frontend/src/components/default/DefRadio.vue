c9ab46be0b06c9aa609bbb63772d66519c9a9bee
SaYMooN <shokoladnysaymoon@mail.ru>
2023-11-01 23:34:36 +0300

    started working on the furnace recipes creation page

---
+++ frontend/src/components/default/DefRadio.vue
@@ -0,0 +1,81 @@
+<template>
+    <label class="default-radio-container">
+    <input class="default-radio" type="radio" :name="name" :value="value" :checked="isChecked"> 
+        <span class="default-radio-label">{{ spanText }}</span>
+    </label>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: String,
+            required: true
+        },
+        isChecked: {
+            type: Boolean,
+            default: false
+        },
+        spanText: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+.default-radio {
+    display: none;
+}
+
+.default-radio-container {
+    padding-left: calc(24px + 0.55vw + 0.55vh);
+    position: relative;
+    cursor: pointer;
+    width:100%;
+    display: flex;
+    align-items: center;
+}
+
+.default-radio-label::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: calc(10px + 0.5vw + 0.5vh);
+    height: calc(10px + 0.5vw + 0.5vh);
+    border: calc(2px + 0.05vw + 0.05vh) solid transparent;
+    border-radius: 50%;
+    background-color: var(--back-3);
+}
+.default-radio-label {
+    color: var(--front);
+    font-family: 'Figtree';
+    font-weight: 600;
+    font-size: calc(1vw + 7px);
+    white-space: nowrap;
+}
+
+.default-radio-container:hover .default-radio-label::before {
+    border-color: var(--front-2);
+}
+
+.default-radio-container:hover .default-radio-label {
+    color: var(--front-2);
+}
+
+.default-radio:checked + .default-radio-label {
+    color: var(--front-3);
+}
+
+    .default-radio:checked + .default-radio-label::before {
+        border-color: var(--front-3);
+    }
+
+</style>
--- frontend/src/components/projectPageTabs/recipes/creationTab/components/Furnace.vue
+++ frontend/src/components/projectPageTabs/recipes/creationTab/components/Furnace.vue
@@ -1,65 +1,86 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-        <p class="input-line">
-            <label class="default-input-label"> input: </label>
-            <input class="default-input" type="text" v-model="inputValue">
-        </p>
-        <p class="input-line">
-            <label class="default-input-label"> output: </label>
-            <input class="default-input" type="text" v-model="outputValue">
-        </p>
-        <div class="furnace-radios-container">
-            <label class="furnace-additional-type-label">Additional</label>
-            <label v-for="type in radioTypes" :key="type.value" class="default-radio-container" >
-                <input class="default-radio" type="radio" name="furnace-type-choice" :value="type.value"
-                    v-model="specialTypeValue">
-                <span class="default-radio-label">{{ type.label }}</span>
-            </label>
-        </div>
-        <label class="default-error-label"></label>
-        <p class="input-line">
-            <input class="default-submit" type="submit" :value="submitButtonText">
-        </p>
+    <form @submit.prevent="handleSubmit" class="form-container">
+        <DefLine labelText="input:">
+            <InputWithSuggestions :value="initialInput" @updateValue="this.inputValue = $event" suggestion-type="item" />
+        </DefLine>
+        <DefLine labelText="output:">
+            <InputWithSuggestions :value="initialOutput" @updateValue="this.outputValue = $event" suggestion-type="item" />
+        </DefLine>
+        <DefLine labelText="additional:" >
+            ${radiosHtml}
+        </DefLine>
+        <DefSave :submitText="submitText" />
     </form>
+    <InvalidInputDialog ref="errDialog" :errorText="`${errDialogText}`"></InvalidInputDialog>
 </template>
   
 <script>
+import DefLine from './../../../../default/DefLine.vue';
+import InputWithSuggestions from './../../../../default/InputWithSuggestions.vue';
+import DefInputNum from './../../../../default/DefInputNum.vue';
+import DefSave from './../../../../default/DefSave.vue';
+import InvalidInputDialog from './../../../../modalDialogs/InvalidInputDialog.vue';
 export default {
-    props: ['formArguments', 'actionId', 'path'],
+    props: {
+        initialInput: {
+            type: String,
+            default: ''
+        },
+        initialOutput: {
+            type: String,
+            default: ''
+        },
+        initialFurnaceType: {
+            type: String,
+            default: "None"
+        },
+        isNew: {
+            type: Boolean,
+            default: true
+        },
+        filePath: {
+            type: String,
+            default: ''
+        },
+        actionId: {
+            type: Number,
+            default: 0
+        },
+    },
     data() {
         return {
-            radioTypes: [
-                { value: "FurnaceOnly", label: "None" },
-                { value: "FurnaceAndSmoker", label: "Smoker" },
-                { value: "FurnaceAndBlast", label: "Blast Furnace" }
-            ]
+            inputValue: this.initialInput,
+            outputValue: this.initialOutput,
+            initialFurnaceTypeValue: this.initialFurnaceType,
+            errDialogText: '',
         };
     },
     computed: {
-        inputValue() {
-            return this.formArguments && this.formArguments.input ? this.formArguments.input : '';
-        },
-        outputValue() {
-            return this.formArguments && this.formArguments.output ? this.formArguments.output : '';
-        },
-        specialTypeValue: {
-            get() {
-                return this.formArguments && this.formArguments.specialType ? this.formArguments.specialType : '';
-            },
-            set(value) {
-                this.$emit('update:specialTypeValue', value);
-            }
-        },
-        submitButtonText() {
-            return this.formArguments ? "Save changes" : "Save to file";
+        submitText() {
+            return this.isNew ? 'Save to file' : 'Save changes';
         }
     },
     methods: {
-        handleSubmit(event) {
+        handleSubmit() {
         }
+    },
+    inject: ['newFurnaceRecipeSaved'],
+    components: {
+        DefLine,
+        InputWithSuggestions,
+        DefInputNum,
+        DefSave,
+        InvalidInputDialog
     }
 }
 </script>
   
-<style scoped></style>
+<style scoped>
+.form-container {
+    padding-top: calc(2vw + 10px + 20vh);
+    padding-left: calc(4vw + 20px + 2vh);
+    width: calc(100% - 4vw - 20px - 2vh);
+    display: block;
+}
+</style>
   
