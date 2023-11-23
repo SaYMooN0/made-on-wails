9d6e5569691c774d1500569bcc69021e8ab0c99f
SaYMooN <shokoladnysaymoon@mail.ru>
2023-11-23 19:52:33 +0300

    Continued working on the tab with visual settings

--- frontend/src/components/settingsPageComponents/tabs/VisualSettingsTab.vue
+++ frontend/src/components/settingsPageComponents/tabs/VisualSettingsTab.vue
@@ -3,7 +3,8 @@
         <div class="chosen-theme-zone">
             <label class="chosen-theme-name">Theme: {{ chosenTheme.Name }}</label>
             <div class="color-labels-container">
-                <ColorLabel v-for="(value, key) in chosenThemeColorFields" :key="key" :label="key" :color="value" />
+                <ColorLabel v-for="(value, key) in chosenThemeColorFields" :key="key" :label="key" :color="value"
+                    ref="colorLabels" />
             </div>
 
         </div>
@@ -12,7 +13,19 @@
             <div class="themes-labels-container">
                 <ThemeLabel v-for="theme in avaliableThemes" :theme="theme" />
             </div>
-            <button class="new-theme-button">Create new theme</button>
+            <button class="new-theme-button">
+                Create new theme
+                <svg viewBox="0 0 24 24" fill="none" class="brush-icon">
+                    <path
+                        d="M9.5 19.5V18H4.5C3.95 18 3.45 17.78 3.09 17.41C2.72 17.05 2.5 16.55 2.5 16C2.5 14.97 3.3 14.11 4.31 14.01C4.37 14 4.43 14 4.5 14H19.5C19.57 14 19.63 14 19.69 14.01C20.17 14.05 20.59 14.26 20.91 14.59C21.32 14.99 21.54 15.56 21.49 16.18C21.4 17.23 20.45 18 19.39 18H14.5V19.5C14.5 20.88 13.38 22 12 22C10.62 22 9.5 20.88 9.5 19.5Z"
+                        stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path
+                        d="M20.17 5.3L19.69 14.01C19.63 14 19.57 14 19.5 14H4.50001C4.43001 14 4.37001 14 4.31001 14.01L3.83001 5.3C3.65001 3.53 5.04001 2 6.81001 2H17.19C18.96 2 20.35 3.53 20.17 5.3Z"
+                        stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M7.98999 2V7" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M12 2V4" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg></button>
         </div>
     </div>
 </template>
@@ -45,6 +58,13 @@ export default {
                 document.documentElement.style.setProperty('--warning-main', theme.WarningMainColor);
                 document.documentElement.style.setProperty('--warning-bright', theme.WarningBrightColor);
             });
+        },
+        cancelAllEditing() {
+            if (this.$refs.colorLabels) {
+                this.$refs.colorLabels.forEach(colorLabelComponent => {
+                    colorLabelComponent.cancelEditing();
+                });
+            }
         }
     },
     computed: {
@@ -66,7 +86,12 @@ export default {
     {
         ColorLabel,
         ThemeLabel
-    }
+    },
+    provide() {
+        return {
+            cancelColorsEditing: this.cancelAllEditing,
+        }
+    },
 }
 </script>
 <style scoped>
@@ -107,15 +132,18 @@ export default {
     border-radius: 2%;
     display: grid;
     grid-template-rows: repeat(11, 1fr);
-    padding: 4%;
+    padding: 2.5%;
     overflow-y: auto;
 }
 
 .avaliable-themes-zone {
-    height: 100%;
+    height: calc(92% - 30px);
     width: 94%;
     padding-left: 3%;
     padding-right: 3%;
+    display: grid;
+    grid-template-rows: auto 1fr calc(23px + 1.6vh + 0.8vw);
+
 }
 
 .avaliable-themes-label {
@@ -126,6 +154,46 @@ export default {
     margin-bottom: 4px;
     font-size: calc(0.9vh + 1.2vw + 9px);
     text-align: center;
-    border-bottom: 2px var(--back-2) solid;
+    border-bottom: 1px var(--back-2) solid;
+}
+
+.themes-labels-container {
+    max-height: 100%;
+    overflow-y: auto;
+}
+
+.new-theme-button {
+
+    height: 100%;
+    justify-self: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--bright-2);
+    color: var(--front);
+    font-family: 'Figtree';
+    font-size: calc(0.55vh + 0.55vw + 11px);
+    border: 1px solid transparent;
+    border-radius: calc(3px + 0.09vw + 0.1vh);
+    padding: calc(0.5% + 3px);
+    cursor: pointer;
+    gap: 3px;
+    transition: 0.08s;
+
+}
+
+.new-theme-button:hover {
+    background-color: var(--bright-3);
+
+}
+
+.brush-icon {
+    height: calc(0.6vh + 0.6vw + 12px);
+    aspect-ratio: 1/1;
+}
+
+.brush-icon path {
+    stroke: var(--front);
+    stroke-width: 2;
 }
 </style>
---
+++ frontend/src/components/settingsPageComponents/tabsComponents/IconButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="svg-container">
+        <slot></slot>
+    </div>
+</template>
+<style scoped>
+.svg-container {
+    height: 100%;
+    aspect-ratio: 1/1;
+}
+</style>
