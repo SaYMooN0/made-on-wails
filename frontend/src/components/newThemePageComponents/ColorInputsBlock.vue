91896c86f981833a911654864ae5eec432c4bad0
SaYMooN <shokoladnysaymoon@mail.ru>
2023-11-30 21:24:22 +0300

    continued to work on the page for creating a new theme

---
+++ frontend/src/components/newThemePageComponents/ColorInputsBlock.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="color-inputs-block">
+        <label class="color-block-label">{{ title }}</label>
+        <NewThemeColorInput v-for="item in colorList" :key="item.label" :label="item.label" :color="item.color" />
+    </div>
+</template>
+  
+<script>
+import NewThemeColorInput from './NewThemeColorInput.vue';
+
+export default {
+    components: {
+        NewThemeColorInput
+    },
+    props: {
+        title: String,
+        type: String,
+        colorList: {
+            type: Array,
+            default: () => []
+        }
+    }
+};
+</script>
+  
+<style scoped>
+.color-block-label {
+    width: 100%;
+    text-align: center;
+    color: #121212;
+    font-family: 'Figtree';
+    font-size: calc(0.5vh + 1.1vw + 8px);
+}
+
+.color-inputs-block {
+    height: calc(100px + 30% + 8vw);
+    width: 100%;
+    background-color: #f5f5f5;
+    border-radius: calc(0.3vh + 0.3vw + 2px);
+    display: grid;
+    grid-template-rows: auto 1fr 1fr 1fr;
+    padding-top: calc(2px + 2vh);
+    align-content: center;
+}
+</style>
+  
--- frontend/src/components/pages/NewThemePage.vue
+++ frontend/src/components/pages/NewThemePage.vue
@@ -1,43 +1,42 @@
 <template>
     <div class="page-container">
-
-
         <h1 class="new-theme-name">
-            New theme name:
-            <input type="text" />
+            <div style="display: flex;justify-content: left; gap: 1vw;align-items: center;">
+                New theme name:
+                <input type="text" class="theme-name-input" />
+            </div>
+            <div class="save-theme-button">Save</div>
         </h1>
         <div class="color-inputs-blocks-container">
-            <div class="color-inputs-block">
-                <label class="color-group-label">Back Colors:</label>
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-            </div>
-            <div class="color-inputs-block">
-                <label class="color-group-label">Front Colors:</label>
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-            </div>
-            <div class="color-inputs-block">
-                <label class="color-group-label">Bright Colors:</label>
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-            </div>
-            <div class="color-inputs-block">
-                <label class="color-group-label">Warning Colors:</label>
-                <NewThemeColorInput />
-                <NewThemeColorInput />
-            </div>
+            <ColorInputsBlock title="Back Colors" type="Back" :colorList="[
+                { label: 'Main', color: '#12ffbb' },
+                { label: 'Second', color: '#34ffdd' },
+                { label: 'Third', color: '#56ffee' }
+            ]" />
+            <ColorInputsBlock title="Front Colors" type="Front" :colorList="[
+                { label: 'Main', color: '#12ffbb' },
+                { label: 'Second', color: '#34ffdd' },
+                { label: 'Third', color: '#56ffee' }
+            ]" />
+            <ColorInputsBlock title="Bright Colors" type="Bright" :colorList="[
+                { label: 'Main', color: '#12ffbb' },
+                { label: 'Second', color: '#34ffdd' },
+                { label: 'Third', color: '#56ffee' }
+            ]" />
+            <ColorInputsBlock title="Warning Colors" type="Warning" :colorList="[
+                { label: 'Main', color: '#12ffbb' },
+                { label: 'Bright', color: '#34ffdd' }
+            ]" />
         </div>
         <GoBackButton @goBack="backToMain" />
     </div>
+    <ErrorDialog ref="errDialog" :errorText="`${errDialogText}`" />
 </template>
 <script>
-import NewThemeColorInput from '../newThemePageComponents/NewThemeColorInput.vue';
+import ColorInputsBlock from '../newThemePageComponents/ColorInputsBlock.vue'
 import GoBackButton from '../GoBackButton.vue';
-import { GetHexDefaultDark } from '../../../wailsjs/go/themerelated/ThemeCollection'
+import { GetHexDefaultDark } from '../../../wailsjs/go/themerelated/ThemeCollection';
+import ErrorDialog from '../modalDialogs/ErrorDialog.vue';
 export default {
     data() {
         return {
@@ -54,7 +53,7 @@ export default {
     },
     components:
     {
-        NewThemeColorInput,
+        ColorInputsBlock,
         GoBackButton
     },
     methods: {
@@ -74,34 +73,67 @@ export default {
     display: grid;
     grid-template-rows: auto 1fr;
     height: 100vh;
-    padding-left: calc(1vh + 0.4vw + 11px);
-    padding-right: calc(1vh + 0.4vw + 11px);
+    padding-left: 4vw;
+    padding-right: 4vw;
 }
 
 .new-theme-name {
     color: var(--front);
     font-family: 'Figtree';
-    font-size: calc(1.4vh + 1.4vw + 11px);
+    font-size: calc(1vh + 1.6vw + 11px);
+    margin-top: calc(4vh + 2vw + 10px);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
 }
 
-.theme-name-input {}
+.theme-name-input {
+    background-color: var(--back-2);
+    border: 1px solid transparent;
+    border-radius: calc(2px + 0.1vw + 0.1vh);
+    font-family: 'Figtree';
+    font-size: calc(0.8vh + 1vw + 9px);
+    font-weight: 300;
+    color: var(--front);
+    padding-left: calc(1px + 0.2vw + 0.2vh);
+    padding-right: calc(1px + 0.2vw + 0.2vh);
+    width: 44%;
+}
+
+.theme-name-input:focus {
+    outline: none;
+    background-color: var(--back-3);
+}
+
+.save-theme-button {
+    height: calc(10px + 50%);
+    background-color: var(--bright-2);
+    border: 1px solid transparent;
+    border-radius: calc(3px + 0.09vw + 0.1vh);
+    font-family: 'Figtree';
+    font-size: calc(0.5vh + 0.6vw + 10px);
+    color: var(--front);
+    padding-left: calc(5px + 0.3vw + 0.3vh);
+    padding-right: calc(5px + 0.3vw + 0.3vh);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: 0.08s;
+}
+
+.save-theme-button:hover {
+    transform: scale(1.02);
+    background-color: var(--bright-3);
+}
 
 .color-inputs-blocks-container {
-    background-color: aqua;
     height: 90%;
     width: 100%;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: calc(0.4vh + 1vw);
     align-items: center;
-}
-
-.color-inputs-block {
-    height: 82%;
-    width: 100%;
-    background-color: #f5f5f5;
-    border-radius: 2%;
-    display: grid;
-    grid-template-rows: auto 1fr 1fr 1fr;
 }
 </style>
